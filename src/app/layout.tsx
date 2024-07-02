99a76eb9841ab0a589a2306f194753ee1377c394
Adrien Bird <adrien@adrienbird.com>
2024-07-02 14:41:40 -0500

    feat: add medusaProvider to rootLayout to enable medusa-react package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next"
 import "styles/globals.css"
+import { MedusaProvider } from "medusa-react"
+import { QueryClient } from "@tanstack/react-query"
+import React from "react"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 
@@ -7,11 +10,18 @@ export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
 }
 
+const queryClient = new QueryClient()
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
-        <main className="relative">{props.children}</main>
+        <MedusaProvider
+          queryClientProviderProps={{ client: queryClient }}
+          baseUrl={process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:9000"}
+        >
+          <main className="relative">{props.children}</main>
+        </MedusaProvider>
       </body>
     </html>
   )

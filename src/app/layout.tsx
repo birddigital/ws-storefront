acb056c60eb9d24037549f4693721d10a553ef79
Adrien Bird <adrien@adrienbird.com>
2024-07-02 15:14:41 -0500

    fix: client query required use-client directive, created client side component to serve as new medusa provider wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import React from "react"
 import { Metadata } from "next"
 import "styles/globals.css"
-import { MedusaProvider } from "medusa-react"
-import { QueryClient } from "@tanstack/react-query"
+import MedusaClientProvider from "../providers/MedusaClientProvider"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 
@@ -10,18 +9,15 @@ export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
 }
 
-const queryClient = new QueryClient()
-
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
-        <MedusaProvider
-          queryClientProviderProps={{ client: queryClient }}
-          baseUrl={BASE_URL}
-        >
+        <MedusaClientProvider>
+          {" "}
+          {/* Use the client component here */}
           <main className="relative">{props.children}</main>
-        </MedusaProvider>
+        </MedusaClientProvider>
       </body>
     </html>
   )
